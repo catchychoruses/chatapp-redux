ca2ab7db51ed4a0b52e90a3070c43f0a2253482b
catchychoruses <catchychoruses@gmail.com>
2023-11-03 09:02:08 +0100

    fix packages

--- frontend/cypress/e2e/spec.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('');
-
-    cy.get('[data-testid="auth-email-field"]').type('smthng');
-    cy.get('[data-testid="auth-password-field"]').type('smthng');
-    cy.get('[data-testid="submit"]').click();
-  });
-});
